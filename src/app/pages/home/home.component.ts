d5d3c11b85a601372439d224af89be61d4fca7c6
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-18 01:42:19 -0700

    moviesService injected, movies response assigned

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/interfaces/nowplaying-response';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  movies: Movie[] = [];
 
-  constructor() { }
+  constructor(private moviesService: MoviesService) {}
 
   ngOnInit(): void {
+    this.moviesService.getNowPlaying().subscribe((response) => {
+      this.movies = response.results;
+    });
   }
-
 }
