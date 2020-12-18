173a641f60733366c718e3be9b8248caaa8e11a9
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-18 01:35:12 -0700

    AppRouting, Components and Pages modules imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ComponentsModule } from './components/components.module';
+import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ComponentsModule,
+    PagesModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
