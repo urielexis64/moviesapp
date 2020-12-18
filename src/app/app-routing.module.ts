7311e987367da9799ea7f5558e9fbb3f87d044ef
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-18 01:35:47 -0700

    App routing (home, movie, search)

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MovieComponent } from './pages/movie/movie.component';
+import { SearchComponent } from './pages/search/search.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'movie/:id',
+    component: MovieComponent,
+  },
+  {
+    path: 'search/:text',
+    component: SearchComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  },
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
