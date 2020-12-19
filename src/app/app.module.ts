7a16c42ec86de60ca8e6a1be5e0aee0674f83bcf
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-19 01:50:03 -0700

    Rating module imported

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PosterGridMoviesComponent } from './poster-grid-movies/poster-grid-movies.component';
+import { RatingModule } from 'ng-starrating';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,7 @@ import { PosterGridMoviesComponent } from './poster-grid-movies/poster-grid-movi
     SlideshowComponent,
     PosterGridMoviesComponent,
   ],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, RatingModule],
   exports: [NavbarComponent, SlideshowComponent, PosterGridMoviesComponent],
 })
 export class ComponentsModule {}
