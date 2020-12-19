ed6f84913356590c198305c03e431d611c7fa02a
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-19 01:53:57 -0700

    dynamic API params

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -2,16 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { NowPlayingResponse } from '../interfaces/nowplaying-response';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
+  private urlBase = 'https://api.themoviedb.org/3';
+  private page: number = 1;
   constructor(private http: HttpClient) {}
 
+  get params() {
+    return {
+      api_key: 'd278c593d23e539bae7d6db38e2cbebd',
+      language: 'es-ES',
+      page: this.page.toString(),
+    };
+  }
+
   getNowPlaying(): Observable<NowPlayingResponse> {
-    return this.http.get<NowPlayingResponse>(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=d278c593d23e539bae7d6db38e2cbebd&language=es-ES&page=1`
-    );
+    return this.http
+      .get<NowPlayingResponse>(`${this.urlBase}/movie/now_playing`, {
+        params: this.params,
+      })
+      .pipe(
+        tap(() => {
+          this.page++;
+        })
+      );
   }
 }
