313d360709fe8ebc32753fa1eff7a53345f93ee5
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-18 22:47:36 -0700

    posterGridMovies declarated and exported

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
 import { SlideshowComponent } from './slideshow/slideshow.component';
+import { PosterGridMoviesComponent } from './poster-grid-movies/poster-grid-movies.component';
 
 @NgModule({
-  declarations: [NavbarComponent, SlideshowComponent],
+  declarations: [
+    NavbarComponent,
+    SlideshowComponent,
+    PosterGridMoviesComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, SlideshowComponent],
+  exports: [NavbarComponent, SlideshowComponent, PosterGridMoviesComponent],
 })
 export class ComponentsModule {}
