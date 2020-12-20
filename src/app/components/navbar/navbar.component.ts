1e75dcdab13566a9b412e319e563dc2b68c181da
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-20 00:29:44 -0700

    searchMovie method

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  searchMovie(term: string) {
+    term = term.trim();
+    if (term.length !== 0) {
+      this.router.navigateByUrl('search/' + term);
+    }
   }
-
 }
