7a1ac057a9c2808523fc902ac56598902fee5e65
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-20 00:25:31 -0700

    search component implemented

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../../services/movies.service';
+import { Movie } from '../../interfaces/nowplaying-response';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
+  movies: Movie[] = [];
+  currentSearch: string;
+  loading: boolean = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private moviesService: MoviesService
+  ) {
+    this.loading = true;
+    activatedRoute.params.subscribe((params) => {
+      //TODO: call service
+      this.currentSearch = params.text;
+      moviesService.searchMovies(params.text).subscribe((response) => {
+        this.movies = response;
+        this.loading = false;
+      });
+    });
   }
 
+  ngOnInit(): void {}
 }
