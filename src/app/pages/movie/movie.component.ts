f000c94cf7f7d3086d755f8fd1bc91046806be9d
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-20 00:37:31 -0700

    Movie component

--- src/app/pages/movie/movie.component.ts
+++ src/app/pages/movie/movie.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MoviesService } from '../../services/movies.service';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
-  styleUrls: ['./movie.component.css']
+  styleUrls: ['./movie.component.css'],
 })
 export class MovieComponent implements OnInit {
+  id: string;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private moviesService: MoviesService
+  ) {}
 
   ngOnInit(): void {
+    const { id } = this.activatedRoute.snapshot.params;
+    this.moviesService.getMovieDetail(id).subscribe((response) => {
+      console.log(response);
+    });
   }
-
 }
