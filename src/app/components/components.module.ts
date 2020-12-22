5a8d5b641f84d33eb0db1f21441ccdaf4ed312ff
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-21 19:46:10 -0700

    CastSlideshow imported and exported

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -6,14 +6,21 @@ import { SlideshowComponent } from './slideshow/slideshow.component';
 import { PosterGridMoviesComponent } from './poster-grid-movies/poster-grid-movies.component';
 import { RatingModule } from 'ng-starrating';
 import { PipesModule } from '../pipes/pipes.module';
+import { CastSlideshowComponent } from './cast-slideshow/cast-slideshow.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
     SlideshowComponent,
     PosterGridMoviesComponent,
+    CastSlideshowComponent,
   ],
   imports: [CommonModule, RouterModule, RatingModule, PipesModule],
-  exports: [NavbarComponent, SlideshowComponent, PosterGridMoviesComponent],
+  exports: [
+    NavbarComponent,
+    SlideshowComponent,
+    PosterGridMoviesComponent,
+    CastSlideshowComponent,
+  ],
 })
 export class ComponentsModule {}
