862d70c10d67d9078429e773f6a0e9a17e813df1
Uriel Aispuro <urielalexis64@gmail.com>
2020-12-21 19:52:16 -0700

    Cast Response interface

---
+++ src/app/interfaces/cast-response.ts
@@ -0,0 +1,31 @@
+export interface CastResponse {
+  id: number;
+  cast: Cast[];
+  crew: Cast[];
+}
+
+export interface Cast {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: Department;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: null | string;
+  cast_id?: number;
+  character?: string;
+  credit_id: string;
+  order?: number;
+  department?: Department;
+  job?: string;
+}
+
+export enum Department {
+  Acting = 'Acting',
+  Camera = 'Camera',
+  Crew = 'Crew',
+  Directing = 'Directing',
+  Production = 'Production',
+  Writing = 'Writing',
+}
